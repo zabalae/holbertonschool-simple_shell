d92f267e7aa8efce49873a07fc3896da4750965d
Elizabeth <elizabeth.zabala3@gmail.com>
2023-12-12 13:03:44 -0400

    fix name conflict

--- main.h
+++ main.h
@@ -7,7 +7,7 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 
-void execute_command(char *cmd, char *self);
+void execute_comm(char *cmd, char *self);
 char *getInput(void);
 void print_environment(void);
 
