14209206bffc609a008a3c10912697a9ee55773b
Elizabeth <elizabeth.zabala3@gmail.com>
2023-12-13 11:20:31 -0400

    test

--- funcs.c
+++ funcs.c
@@ -10,14 +10,23 @@ char *getInput(void)
 	size_t size = 32, len;
 	ssize_t errCheck;
 
+	input = malloc(size *sizeof(char));
+	if (input == NULL)
+	{
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
+
 	errCheck = getline(&input, &size, stdin);
 	if (errCheck == -1)
 	{
 		if (isatty(STDIN_FILENO))
 		{
 			perror("getline");
+			free(input);
 			exit(EXIT_FAILURE);
 		}
+		free(input);
 		exit(EXIT_SUCCESS);
 	}
 	len = strlen(input);
@@ -36,44 +45,55 @@ char *getInput(void)
 void execute(char *cmd, char *self, char *envp[])
 {
 	int i = 0;
-	char *cmd_args[259];
+	char **cmd_args = malloc(258 * sizeof(char *));
 	pid_t pid;
+	char *cmd_copy;
 
-	if (strcmp(cmd, "env") == 0)
+	if (cmd_args == NULL)
 	{
-		print_enviroment(envp);
-		return;
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
+
+	cmd_copy = strdup(cmd);
+
+	cmd_args[i] = strtok(cmd_copy, " ");
+
+	while (cmd_args[i] != NULL)
+	{
+		i++;
+		if (i >= 258)
+		{
+			printf("Command Fail, Too many arguments. Max 258.\n");
+			free(cmd_copy);
+			free(cmd_args);
+			return;
+		}
+		cmd_args[i] = strtok(NULL, " ");
+	}
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		free(cmd_copy);
+		free(cmd_args);
+		exit(EXIT_FAILURE);
+	}
+	if (pid == 0)/*child process*/
+	{
+		execve(cmd_args[0], cmd_args, envp);
+		printf("%s: 1: %s: not found\n", self, cmd);
+		fflush(stdout);
+		free(cmd);
+		free(cmd_args);
+		exit(EXIT_FAILURE);
 	}
 	else
 	{
-		cmd_args[i] = strtok(cmd, " ");
-		while (cmd_args[i] != NULL)
-		{
-			i++;
-			if (i >= 258)
-			{
-				printf("Command Fail, Too many arguments. Max 258.\n");
-				return;
-			}
-			cmd_args[i] = strtok(NULL, " ");
-		}
-		pid = fork();
-		if (pid == -1)
-		{
-			perror("fork");
-			exit(EXIT_FAILURE);
-		}
-		if (pid == 0)/*child process*/
-		{
-			execve(cmd_args[0], cmd_args, envp);
-			printf("%s: 1: %s: not found\n", self, cmd);
-			fflush(stdout);
-			free(cmd);
-			exit(EXIT_FAILURE);
-		}
-		else
-			wait(NULL);
+		wait(NULL);
 	}
+	free(cmd_copy);
+	free(cmd_args);
 }
 /**
 * print_enviroment - for env command enviroment
