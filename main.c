ae1ba69c7ab795ffdab0a3a73a0bee1975c9e0a9
William <titanbabrisa@gmail.com>
2023-12-13 01:31:46 -0400

    fix exit bug

--- main.c
+++ main.c
@@ -2,20 +2,19 @@
 
 int main(int argc, char *argv[])
 {
-	char *input = NULL, *prevInput;
+	char *input = NULL, *prevInput = NULL;
 	(void)argc;
 
 	do {/*infinit loop till exit is input*/
-		prevInput = input;
 		if (isatty(STDIN_FILENO))
 			printf("$ ");
 		input = getInput();/*use getline for input*/
 		if (strcmp(input, "exit") != 0)
 		{
-			if (input != NULL)
-				execute(input, argv[0]);
+			execute(input, argv[0]);
 		}
 		free(prevInput);/*deals with leaks*/
+		prevInput = input;
 	} while (strcmp(input, "exit") != 0);
 	free(input);/*getline allocate memory, needs free*/
 	return (0);
