550983ea09aadf8579d6e729c68845ff70b5ddee
William <titanbabrisa@gmail.com>
2023-12-18 08:23:42 -0400

    added comments

--- main.c
+++ main.c
@@ -11,29 +11,29 @@ int main(int argc, char *argv[], char *envp[])
 {
 	int flag = 1, executes = 0;
 	char *cmd = NULL;
-	(void)argc;
+	(void)argc;/*set argc to void, not been use*/
 
 	do {/*infinit loop till exit is input*/
 		while (flag == 1)
 		{
-			if (isatty(STDIN_FILENO))
+			if (isatty(STDIN_FILENO))/*check for interactive use*/
 				printf("$ ");
 			cmd = getInput();/*use getline for input*/
-			flag = spaceChecker(cmd);
-			if (flag == 1)
+			flag = spaceChecker(cmd);/*here we check for spaces*/
+			if (flag == 1)/*if flags is 1 means only spaces found*/
 				free(cmd);
 		}
 		if (cmd != NULL)
 		{
-			if (strcasecmp(cmd, "exit") == 0)
+			if (strcasecmp(cmd, "exit") == 0)/*verify usr input*/
 			{
 				free(cmd);
-				if (executes > 0)
+				if (executes > 0)/*check if shell has run*/
 					exit(2);
 				break;
 			}
 			else
-			{
+			{/*depending on the input calls desire function*/
 				if (strcmp(cmd, "env") != 0)
 					execute(cmd, argv[0], envp);
 				else
@@ -41,8 +41,8 @@ int main(int argc, char *argv[], char *envp[])
 				free(cmd);
 			}
 		}
-		executes++;
-		flag = 1;
+		executes++;/*adds every time shell run*/
+		flag = 1;/*resets back to 1 to re-check for white spaces*/
 	} while (1);
 	return (0);
 }
